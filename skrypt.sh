5aad4672ef42e1bd7a68bf9ed9e78e9d8ae39f30
Dawid51513 <129769614+Dawid51513@users.noreply.github.com>
2023-04-03 22:11:02 +0200

    --logs tworzy 100 plikow

--- skrypt.sh
+++ skrypt.sh
@@ -6,4 +6,12 @@ function data {
 
 if [ "$1" = "--date" ]; then
 echo $(data)
+elif [ "$1" = "--logs" ]; then
+ for (( i = 1 ; i <= 100; i++ ))
+ do
+ touch log$i.txt
+ echo  "$i" "log$i.txt"  >>log$i.txt
+ echo "$0" >>log$i.txt
+ echo $(data) >>log$i.txt
+ done
 fi
