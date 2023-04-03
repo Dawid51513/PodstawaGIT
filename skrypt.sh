3a394a2d635acd0add21d17d96b12c8af54aa01b
Dawid51513 <129769614+Dawid51513@users.noreply.github.com>
2023-04-04 00:14:59 +0200

    to co log ale z error

--- skrypt.sh
+++ skrypt.sh
@@ -34,4 +34,21 @@ elif [ "$1" = "--init" ] || [ "$1" = "-i" ]; then
 git clone https://github.com/Dawid51513/PodstawaGIT
 export PATH="$PATH:$(dirname "$0")"
 
+elif [ "$1" = "--error" ] || [ "$1" = "-e" ]; then
+ if [ "$2" ]; then
+  for (( i=1; i<=$2; i++ )); do
+   mkdir error$i
+   echo "$i" "error$i.txt"  >>error$i/error$i.txt
+   echo "$0" >>error$i/error$i.txt
+   echo $(data) >>error$i/error$i.txt
+ done
+
+ else
+  for (( i=1; i<=100; i++ )); do
+  mkdir log$i
+  echo  "$i" "error$i.txt"  >>error$i/error$i.txt
+  echo "$0" >>error$i/error$i.txt
+  echo $(data) >>error$i/error$i.txt
+  done
+ fi
 fi
